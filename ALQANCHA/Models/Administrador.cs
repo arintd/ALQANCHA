f376b5aef661e3f2e9dc82284ee41e067d6706b8
StephaniaGonzalez <nia.gc03@gmail.com>
2024-06-05 20:47:35 -0300

    Validaciones "Administrador"

--- ALQANCHA/Models/Administrador.cs
+++ ALQANCHA/Models/Administrador.cs
@@ -12,15 +12,24 @@ namespace ALQANCHA.Models
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        [StringLength(30, MinimumLength = 5, ErrorMessage = "El nombre debe tener entre 5 y 30 caracteres")]
         public string Nombre { get; set; }
 
+        [Required(ErrorMessage = "El apellido es obligatorio")]
+        [StringLength(30, MinimumLength = 5, ErrorMessage = "El apellido debe tener entre 5 y 30 caracteres")]
         public string Apellido { get; set; }
 
+
+        [Required(ErrorMessage = "El teléfono es obligatorio")]
+        [StringLength(30, MinimumLength = 5, ErrorMessage = "El teléfono debe tener entre 5 y 30 caracteres")]
         public string Telefono { get; set; }
 
+        [Required]
         public bool Activo { get; set; }
 
-        [EmailAddress]
+        [Required(ErrorMessage = "El correo electrónico es obligatorio")]
+        [EmailAddress(ErrorMessage = "Correo electrónico no válido")]
         public string Email { get; set; }
     }
 }
