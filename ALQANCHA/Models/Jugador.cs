8b516b33efb916c75c8539c3ba1f803d866df683
StephaniaGonzalez <nia.gc03@gmail.com>
2024-06-05 21:23:46 -0300

    Validaciones "Jugador"

--- ALQANCHA/Models/Jugador.cs
+++ ALQANCHA/Models/Jugador.cs
@@ -12,19 +12,44 @@ namespace ALQANCHA.Models
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+
+
+        [Required(ErrorMessage = "El DNI es obligatorio")]
+        [StringLength(10, MinimumLength = 7, ErrorMessage = "El DNI debe tener entre 7 y 10 caracteres")]
         public string Dni { get; set; }
+
+
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        [StringLength(30, MinimumLength = 5, ErrorMessage = "El nombre debe tener entre 5 y 30 caracteres")]
         public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "El apellido es obligatorio")]
+        [StringLength(30, MinimumLength = 5, ErrorMessage = "El apellido debe tener entre 5 y 30 caracteres")]
         public string Apellido { get; set; }
-        [EmailAddress]
-        public string Email { get; set; }
+
+        [Required(ErrorMessage = "El teléfono es obligatorio")]
+        [StringLength(30, MinimumLength = 5, ErrorMessage = "El teléfono debe tener entre 5 y 15 caracteres")]
         public string Telefono { get; set; }
+
+        [Required(ErrorMessage = "El correo electrónico es obligatorio")]
+        [EmailAddress(ErrorMessage = "Correo electrónico no válido")]
+        public string Email { get; set; }
+
+        [Display(Name = "¿Es arquero?")]
         public bool EsArquero { get; set; }
+
+        [Display(Name = "¿Es jugador?")]
         public bool EsJugador { get; set; }
+
         [Display(Name = "Fecha Disponible")]
         public DateTime FechaDisponible { get; set; }
+
         [Display(Name = "Hora Disponible")]
         public TimeSpan HoraDisponible { get; set; }
+
+        [Display(Name = "¿Está sancionado?")]
         public bool EstaSancionado { get; set; }
+
         public Sancion Sancion { get; set; }
     }
 }
